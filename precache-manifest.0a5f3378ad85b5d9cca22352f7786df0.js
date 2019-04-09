e9dcee0fab64ba2273c8ccda7fb65025feade471
mohit0201 <31921322+mohit0201@users.noreply.github.com>
2019-04-10 01:32:03 +0530

    Updates

---
+++ precache-manifest.0a5f3378ad85b5d9cca22352f7786df0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21a9596ebfdc045bb707",
+    "url": "/tic-tac-toe/static/css/main.affd96e0.chunk.css"
+  },
+  {
+    "revision": "21a9596ebfdc045bb707",
+    "url": "/tic-tac-toe/static/js/main.cb81a4f0.chunk.js"
+  },
+  {
+    "revision": "72d2a57eca9de8d97851",
+    "url": "/tic-tac-toe/static/js/runtime~main.1fdedca4.js"
+  },
+  {
+    "revision": "f2bf82953ccd6cf2478e",
+    "url": "/tic-tac-toe/static/js/2.087e00c1.chunk.js"
+  },
+  {
+    "revision": "4f0b7025cbc5b9d511b7c531b53ebc17",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
